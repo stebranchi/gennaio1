50d7fe5f1d258a90cc6eb0e409f9abcd1fe65a2d
stebranchi <ste.branchi@gmail.com>
2018-01-24 18:54:51 +0100

    merged getById

--- server.js
+++ server.js
@@ -9,21 +9,33 @@ app.use(bodyparser.urlencoded({extended: true}));
 
 var port = process.env.PORT || 8080;
 
-
+var id=1;
 
 var astronauts=[];
 
 
 app.post('/addAstronauts', (req,res) => {
-  var iUsers= uuid();
-  astronauts.push({"id": iUsers, "name" : req.body.firstName, "surname": req.body.lastName, "state": req.body.isInSpace });
-	res.json({"id": iUsers, "name" : req.body.firstName, "surname": req.body.lastName, "state": req.body.isInSpace });
+  astronauts.push({"id": id, "name" : req.body.firstName, "surname": req.body.lastName, "state": req.body.isInSpace });
+  res.json({"id": id, "name" : req.body.firstName, "surname": req.body.lastName, "state": req.body.isInSpace });
+  id++;
 	res.sendStatus(201);
 });
 
 app.get('/userall', (req,res) => {
 		res.json(astronauts);
 		res.sendStatus(200);
+});
+
+app.get('/user/:id', (req,res) => {
+	var x = req.params.id;
+	var index = astronauts.findIndex(item => {return item.id == x});
+	if(index != -1){
+		res.json(astronauts[index]);
+		res.sendStatus(200);
+	}
+	else{
+		res.sendStatus(404);
+	}
 });
 
 
